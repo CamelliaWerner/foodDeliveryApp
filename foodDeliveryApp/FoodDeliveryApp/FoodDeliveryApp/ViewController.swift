1ccd2e2995cb2c0fad9dde96e59c78349eb52958
Vanya <97010502+CamelliaWerner@users.noreply.github.com>
2025-06-19 00:13:41 +0500

    Add files via upload

---
+++ foodDeliveryApp/FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Иван Кравченко  on 13.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
